49df1ce00d733b20ae0e7841d086f4b46c9ed520
Die Aissatou Dieng <dieaissatou.dieng@segula.fr>
2023-06-23 15:47:03 +0200

    test4

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Bonjour');
 });
+//Bien vu
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
